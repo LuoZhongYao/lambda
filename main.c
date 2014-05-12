2546736e21e1ff173b4cacb4a617fda31411c296
LuoZhongYao <LuoZhongYao@gmail.com>
2014-05-12 10:34:58 +0800

    添加unused,APPLY,EXPR

--- main.c
+++ main.c
@@ -4,24 +4,28 @@
 void (*test_test(void))(void){
     return $(void,(void){printf("000000\n");});
 }
-int main(void){
+
+int main(int argc,char **argv){
     FILE *fd;
     int len;
+
+    unused(argc,argv);
+
     fd = try(NULL == ,fopen("l","w"),
             {
                 perror("Open l");
-                throw(e_open);
+                throw e_open;
             });
     len = try(0 >= ,fwrite("hello",1,5,fd),
             {
                 perror("Write \"hello\" to l");
-                throw(e_write);
+                throw e_write;
             });
     
     len = try(0 >= ,fwrite(" world",1,7,fd),
             {
                 perror("Write \" world\" to l");
-                throw(e_write1);
+                throw e_write1;
             });
 
 
@@ -31,7 +35,7 @@ int main(void){
     if(and(1 <,2,3,4,5,6,-1)){
         printf("H\n");
     }
-    try(0 == ,and(1<,2,3,4,5,or(0 >,-1,-2),6,-1));
+    try(0 == ,and(1 <,2,3,4,5,(or(0 >,-1,-2)),6,-1));
 
     return 0;
     catch(e_write,e_write1){
